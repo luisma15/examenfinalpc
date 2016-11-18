20f1faf100b8f27ad4af7c0cc56666bb6db7ef27
luisma15 <luisma15_banda@hotmail.com>
2016-11-18 11:11:19 -0600

    Version final

--- blib/views.py
+++ blib/views.py
@@ -7,5 +7,28 @@ def listarp(request):
     return render(request, 'blib/listarp.html', {})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return render(request, 'blib/post_edit.html', {'form': form})
+    else:
+        form=PostForm()
     return render(request, 'blib/post_edit.html', {'form': form})
+
+
+#def post_new(request):
+    #if request.method == "POST":
+    #    form = PostForm(request.POST)
+    #    if form.is_valid():
+    #        post = form.save(commit=False)
+    #        #post.author = request.user
+    #        #post.published_date = timezone.now()
+    #        post.save()
+    #        return render('blib/post_edit.html', {'form': form})
+    #else:
+    #    form = PostForm();
+    #return render('blib/post_edit.html', {'form': form})
+#    form = PostForm()
+#    return render(request, 'blib/post_edit.html', {'form': form})
